717e29b3d3ac38b668537fff37413219d1719f55
yancyn <yancyn@gmail.com>
2013-10-15 13:15:12 +0800

    Show as no name when rumi is empty in nisan index.

---
+++ src/HLGranite.Mvc/Models/NisanModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HLGranite.Mvc.Models
+{
+    public partial class Nisan
+    {
+        public override string ToString()
+        {
+            if (String.IsNullOrEmpty(this.Rumi))
+                return "(tiada nama)";
+            else
+                return this.Rumi;
+        }
+    }
+}
