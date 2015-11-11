e5ae9782f875e4a907197de8765fe366eb73aecd
yancyn <yancyn@gmail.com>
2015-11-11 18:32:36 +0800

    Create class MonthVolume to hold chart result

---
+++ src/HLGranite.Mvc/Models/MonthVolume.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HLGranite.Mvc.Models
+{
+    /// <summary>
+    /// To hold total quantity sold in a month for chart use only.
+    /// </summary>
+    public class MonthVolume
+    {
+        public int Month { get; set; }
+        public int Quantity { get; set; }
+        public MonthVolume()
+        {
+        }
+        public MonthVolume(int month, int quantity)
+        {
+            this.Month = month;
+            this.Quantity = quantity;
+        }
+    }
+}
