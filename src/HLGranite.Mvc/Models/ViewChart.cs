0957a6de44cb20f5636b69932466500c96d0d0ea
yancyn <yancyn@gmail.com>
2015-11-13 10:25:11 +0800

    Introduce chart model to hold raw bytes
    Implement display chart in mvc pattern.

---
+++ src/HLGranite.Mvc/Models/ViewChart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HLGranite.Mvc.Models
+{
+    /// <summary>
+    /// A model to hold chart image.
+    /// </summary>
+    public class ViewChart
+    {
+        public byte[] Raw { get; set; }
+        public ViewChart()
+        {
+        }
+        public ViewChart(byte[] bytes)
+        {
+            this.Raw = bytes;
+        }
+    }
+}
